0b3fc503bc8d1682d239b99b42465a7bde6654d6
Abhi1258839 <jindalabhimanyu1@gmail.com>
2026-05-16 12:38:11 -0700

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,12 +1,12 @@
-import scanner.*;
+import java.util.Scanner;
 public class BinarySearch {
     public static int binarySearch(int[] arr, int target) {
         int start =0;
         int end = arr.length-1;
-        System.out.println(arr.length);
+        return arr.length;
     }
     public static void main(String[] args) {
-        //Scanner input = new Scanner(System.in);
-        binarySearch(new int[]{1,2,3,4,5}, 3);
+        Scanner input = new Scanner(System.in);
+        System.out.println(binarySearch(new int[]{1,2,3,4,5}, 3));
     }
 }
