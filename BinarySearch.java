f280e2e5d208181d93384ed1397260a29f5c9b6d
Abhi1258839 <jindalabhimanyu1@gmail.com>
2026-05-16 12:56:10 -0700

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -5,15 +5,19 @@ public class BinarySearch {
         int end = arr.length-1;
         return arr.length;
     }
-    public static void createArray(Scanner i){
-
+    public static void createArray(Scanner i, int[] ar){
+        for(int j =0;j<ar.length;j++){
+            System.out.print("Enter the element at index "+j+": ");
+            ar[j] = i.nextInt();
+        }
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter the length of the array: ");
+        System.out.print("Enter the length of the array: ");
         int length = input.nextInt();
         int[] arr = new int[length];
+        createArray(input, arr);
         input.close();
         //System.out.println(binarySearch());
     }
