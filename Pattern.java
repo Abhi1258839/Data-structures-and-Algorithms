35c3ecfd1ed8c9c76054119f39fdf8967c4ceace
Abhi1258839 <jindalabhimanyu1@gmail.com>
2026-05-16 17:04:20 -0700

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -7,6 +7,7 @@ public class Pattern {
             System.out.println();
         }
     }
+
     public static void patternL2(int r, int c){
         for(int i =0;i <r;i++){
             for(int j = 0; j < c; j++){
@@ -20,11 +21,18 @@ public class Pattern {
             System.out.println();
         }
     }
-
+    
+    public static void patternL3(int r){
+        for(int i =0;i <r;i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         //patternL1(4, 5);
-        System.out.println();
-        patternL2(7, 10);
-
+        //patternL2(7, 10);
+        patternL3(5);
     }
 }
